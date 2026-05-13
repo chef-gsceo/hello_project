e6e811e90bfcd3283be9b736f8087f869df18de3
CHEF <moltern.light@gmail.com>
2026-05-13 21:30:23 +0300

    sort & sorted & reverse

---
+++ countries.py
@@ -0,0 +1,15 @@
+countries = ['usa', 'georgia', 'france', 'canada', 'russia', 'livia']
+
+print(countries)
+print(sorted(countries))
+print(countries)
+print(sorted(countries, reverse=True))
+print(countries)
+countries.reverse()
+print(countries)
+countries.reverse()
+print(countries)
+countries.sort()
+print(countries)
+countries.sort(reverse=True)
+print(countries)
