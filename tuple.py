f080afe16b46f89fc1ce1d615af371ce82d2ad07
chef <moltern.light@gmail.com>
2026-06-28 13:49:43 +0300

    ignore notes linebrakes

---
+++ tuple.py
@@ -0,0 +1,16 @@
+# ДЗ: создать кортеж с 5 блюдами для шведского стола
+# использовать цикл for для вывода всех блюд
+# добавить блок кода который заменяет кортеж, в новом будут изменены два блюда
+# использовать цикл for для вывода всех блюд нового кортежа
+
+meals = ('pork', 'veal', 'tomato', 'chicken', 'pizza')
+
+for meal in meals:
+    print(meal.title())
+
+print('\n')
+
+meals = ('vegetables', 'rice', 'tomato', 'chicken', 'pizza')
+
+for meal in meals:
+    print(meal.title())
